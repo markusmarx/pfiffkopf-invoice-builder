4c77c69390fe3094564394a2b2fe2b96e0098dd7
Fabio Lugert <fabio.lugert0@gmail.com>
2025-04-05 12:33:33 +0200

    Implement basic panning and zooming

--- apps/my-app/src/app/editor/view/view.tsx
+++ apps/my-app/src/app/editor/view/view.tsx
@@ -1,16 +1,69 @@
 import { Container, Paper, ScrollArea } from "@mantine/core";
 import { ViewProperties} from "../types";
-import { useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+class EditorUtil{
+    public static xPos: number;
+    public static yPos: number;
+    public static zoomFactor = 1;
+    public static middleMousePress: boolean;
+    public static overEditor: boolean;
+}
 export function DefaultView(properties: ViewProperties)
 {
-    const xPosition = useState(10);
-    const yPosition = useState(10);
-    const offsetY = `calc(var(--app-shell-header-height, 0px) + ${yPosition}px)`;
-    const offsetX = `calc(var(--app-shell-navbar-width) + ${xPosition}px)`;
+    const editorWindow = useRef<HTMLDivElement | null>(null);
+    const paperElement = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        if(editorWindow.current != null && paperElement.current != null){
+            EditorUtil.xPos = 10;
+            EditorUtil.yPos = 10;
+            document.onmousemove = (event) => {
+                if(!EditorUtil.middleMousePress || !EditorUtil.overEditor)
+                    return;
+                EditorUtil.xPos += event.movementX;
+                EditorUtil.yPos += event.movementY;
+                
+                if(paperElement.current != null){
+                    paperElement.current.style.left = `calc(var(--app-shell-navbar-width) + ${EditorUtil.xPos}px)`;
+                    paperElement.current.style.top = `calc(var(--app-shell-header-height, 0px) + ${EditorUtil.yPos}px)`;
+                }
+    
+            }
+            if(paperElement.current != null){
+                editorWindow.current.onmouseleave = () => {
+                    EditorUtil.overEditor = false;
+                }
+                editorWindow.current.onmouseenter = () => {
+                    EditorUtil.overEditor = true;
+                }
+            }
+            document.onmousedown = () => {
+                EditorUtil.middleMousePress = true;
+            };
+            document.onmouseup = () => {
+                EditorUtil.middleMousePress = false;
+            }
+            document.onwheel = (event) => {
+                if(EditorUtil.overEditor && paperElement.current != null){
+                    if(event.deltaY > 0){
+                        //scroll out
+                        EditorUtil.zoomFactor -= 0.1;
+                    }else{
+                        //scroll in
+                        EditorUtil.zoomFactor += 0.1;
+                    }
+                    EditorUtil.zoomFactor = Math.max(0.1, EditorUtil.zoomFactor);
+                    paperElement.current.style.width = `calc(21cm * ${EditorUtil.zoomFactor})`;
+                    paperElement.current.style.height =  `calc(27cm * ${EditorUtil.zoomFactor})`;
+                }
+                
+            }
+        }
+    }, [editorWindow, paperElement]);
+    const offsetY = `calc(var(--app-shell-header-height, 0px) + ${EditorUtil.yPos}px)`;
+    const offsetX = `calc(var(--app-shell-navbar-width) + ${EditorUtil.xPos}px)`;
     return(
-        <div style={{backgroundColor: "gray", width: "100%", height: "calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px))"}}>
-            <div style={{backgroundColor: "white", width: "21cm", height: "27cm", position: "fixed", top: offsetY, left: offsetX}}>
+        <div ref={editorWindow} style={{backgroundColor: "gray", width: "100%", height: "calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px))"}}>
+            <div ref={paperElement} id="paper" style={{backgroundColor: "white", width: `calc(21cm * ${EditorUtil.zoomFactor})`, height: `calc(27cm * ${EditorUtil.zoomFactor})`, position: "fixed", top: offsetY, left: offsetX}}>
 
             </div>
         </div>
