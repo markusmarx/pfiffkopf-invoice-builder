25e80d1293dcdd9c5bea09628246f3eb60c2ce1b
Fabio Lugert <fabio.lugert0@gmail.com>
2025-08-05 01:10:11 +0200

    Implement prototype resizing tables

--- apps/my-app/src/app/editor/movable/baseMovable.tsx
+++ apps/my-app/src/app/editor/movable/baseMovable.tsx
@@ -1,53 +1,60 @@
 import "./movableBox.module.css";
 import { Rnd } from "react-rnd";
 import { RenderableBlockParams} from "../types";
+import { useRef } from "react";
 export function BaseMovableBox(properties: RenderableBlockParams) {
+  console.log(properties.enabled);
+  const rnd = useRef<Rnd>(null);
   return (
-    <Rnd
-      className={
-        properties.className + (properties.enabled ? " moving-box" : "")
-      }
-      size={{
-        width: `${properties.sizeVector ? properties.sizeVector.x : properties.width || 100}px`,
-        height: `${properties.sizeVector ? properties.sizeVector.y : properties.heigth || 100}px`,
-      }}
-      position={{ x:  properties.posVector ? properties.posVector.x : properties.x || 0, y: properties.posVector ? properties.posVector.y : properties.y || 0 }}
-      disableDragging={!properties.enabled && !properties.disableMovement}
-      enableResizing={properties.enabled && properties.enableResizing}
-      onDragStop={(e, d) => {
-        if (properties.onSubmitPositionChange)
-          properties.onSubmitPositionChange(
-            d.x,
-            d.y,
-            properties.template,
-            properties.templateTab
-          );
-      }}
-      onDrag={(e, d) => {
-        if (properties.onDrag) {
-          properties.onDrag(d.x, d.y, properties.templateTab);
+    
+      <Rnd
+        ref={rnd}
+        className={
+          properties.className + (properties.enabled ? " moving-box" : "")
         }
-      }}
-      bounds={"parent"}
-      onResize={(e, dir, ref, delta, position) => {
-        console.log((ref.style.width.slice(0, -2)));
-        if(properties.onResize){
-            properties.onResize(Number(ref.style.width.slice(0, -2)), Number(ref.style.height.slice(0, -2)), properties.templateTab);
-            if(properties.onDrag){
-                properties.onDrag(position.x, position.y, properties.templateTab);
-            }
-        }
-      }}
-      onResizeStop={(e, dir, ref, delta, position) => {
-        if(properties.onSubmitSizeChange){
-            properties.onSubmitSizeChange(Number(ref.style.width.slice(0, -2)), Number(ref.style.height.slice(0, -2)), properties.template, properties.templateTab);
-            if(properties.onDrag){
-                properties.onDrag(position.x, position.y, properties.templateTab);
-            }
-        }
-      }}
-    >
-        {properties.children}
-    </Rnd>
+        size={{
+          width: `${properties.sizeVector ? properties.sizeVector.x : properties.width || 100}px`,
+          height: `${properties.sizeVector ? properties.sizeVector.y : properties.heigth || 100}px`,
+        }}
+        position={{ x:  properties.posVector ? properties.posVector.x : properties.x || 0, y: properties.posVector ? properties.posVector.y : properties.y || 0 }}
+        disableDragging={!properties.enabled && !properties.disableMovement}
+        enableResizing={properties.enabled && properties.enableResizing}
+        onDragStop={(e, d) => {
+          if (properties.onSubmitPositionChange)
+            properties.onSubmitPositionChange(
+              d.x,
+              d.y,
+              properties.template,
+              properties.templateTab
+            );
+        }}
+        onDrag={(e, d) => {
+          if (properties.onDrag) {
+            properties.onDrag(d.x, d.y, properties.templateTab);
+          }
+        }}
+        cancel=".child_drag"
+        bounds={"parent"}
+        onResize={(e, dir, ref, delta, position) => {
+          if(properties.onResize){
+              properties.onResize(Number(ref.style.width.slice(0, -2)), Number(ref.style.height.slice(0, -2)), properties.templateTab);
+              if(properties.onDrag){
+                  properties.onDrag(position.x, position.y, properties.templateTab);
+              }
+          }
+        }}
+        onResizeStop={(e, dir, ref, delta, position) => {
+          if(properties.onSubmitSizeChange){
+              properties.onSubmitSizeChange(Number(ref.style.width.slice(0, -2)), Number(ref.style.height.slice(0, -2)), properties.template, properties.templateTab);
+              if(properties.onDrag){
+                  properties.onDrag(position.x, position.y, properties.templateTab);
+              }
+          }
+        }}
+        
+      >
+         {properties.children}
+      </Rnd>
+    
   );
 }
--- apps/my-app/src/app/editor/movableTable/movableTable.tsx
+++ apps/my-app/src/app/editor/movableTable/movableTable.tsx
@@ -1,92 +1,157 @@
 import { RenderableBlockParams } from "../types";
 import { Text, Table } from "@mantine/core";
 import { BaseMovableBox } from "../movable/baseMovable";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 export interface MovableTableParams extends RenderableBlockParams {
-  collums: Collumn[]; 
+  collums: Collumn[];
 }
 
-interface ResizableColumnHeaderProps{
+
+interface ResizableColumnHeaderProps {
   col: Collumn;
   width: number;
   onResize: (accesor: string, delta: number) => void;
 }
-export interface Collumn{
+interface ResizableCellProps extends ResizableColumnHeaderProps{
+  value: string;
+}
+
+export interface Collumn {
   accessor: string;
   label: string;
 }
 
+function handleResize(e: React.MouseEvent<HTMLDivElement, MouseEvent>, accessor: string, onResize: (accesor: string, delta: number) => void){
+  let startX = e.clientX;
+  const handleMouseMove = (event : MouseEvent) => {
+      const delta = event.clientX - startX;
+      startX = event.clientX;
+      onResize(accessor, delta);
+      
+  };
+  const handleMouseUp = () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+  };
+  document.addEventListener("mousemove", handleMouseMove);
+  document.addEventListener("mouseup", handleMouseUp);
+}
+
 function ResizableColumnHeader(props: ResizableColumnHeaderProps) {
-    return (
-        <th style={{ width: props.width, position: "relative" }}>
-            {props.col.label}
-        </th>
-    );
-};
+  return (
+    <th
+      style={{
+        width: `${props.width}px`,
+        position: "relative",
+        border: "3px solid",
+
+      }}
+    >
+      {props.col.label}
+      <div
+        className="child_drag"
+        style={{
+            position: "absolute", right: 0, top: 0, height: "100%", width: "5px", cursor: "ew-resize"
+        }}
+        onMouseDown={(e) => handleResize(e, props.col.accessor, props.onResize)}
+      />
+    </th>
+  );
+}
+function ResizableCell(props: ResizableCellProps) {
+  return (
+    <td
+      style={{
+        width: `${props.width}px`,
+        position: "relative",
+        border: "3px solid",
+
+      }}
+    >
+      {props.value}
+      <div
+        className="child_drag"
+        style={{
+            position: "absolute", right: 0, top: 0, height: "100%", width: "5px", cursor: "ew-resize"
+        }}
+        onMouseDown={(e) => handleResize(e, props.col.accessor, props.onResize)}
+      />
+    </td>
+  );
+}
 
 export function MovableTable(properties: MovableTableParams) {
+  const boxWidth = properties.sizeVector
+    ? properties.sizeVector.x
+    : properties.width || 1;
+  const [colWidths, setColWidths] = useState<{ [Key: string]: number }>(
+    properties.collums.reduce(
+      (acc, col) => ({
+        ...acc,
+        [col.accessor]: boxWidth / properties.collums.length / boxWidth,
+      }),
+      {}
+    )
+  );
+  function handleResize (col: string, delta: number){
+    setColWidths((prev) => ({
+      ...prev,
+      [col]: Math.max(prev[col] + delta, 50),
+    }));
+  };
 
-    const [colWidths, setColWidths] = useState<{[Key: string]: number}>(
-          properties.collums.reduce((acc, col) => ({ ...acc, [col.accessor]: 150 }), {})
-      );
-    const handleResize = (col: string, delta : number) => {
-      console.log("resize");
-          setColWidths((prev) => ({
-              ...prev,
-              [col]: Math.max(prev[col] + delta, 50),
-          }));
-      };
-
-
-   return(
-      <BaseMovableBox
-        template={properties.template}
-        templateTab={properties.templateTab}
+  return (
+    <BaseMovableBox
+      template={properties.template}
+      templateTab={properties.templateTab}
+      id={properties.id}
+      enabled={properties.enabled}
+      className={properties.className}
+      enableResizing={properties.enableResizing}
+      width={properties.width}
+      heigth={properties.heigth}
+      sizeVector={properties.sizeVector}
+      autoBreakOverMultiplePages={properties.autoBreakOverMultiplePages}
+      onResize={properties.onResize}
+      onSubmitSizeChange={properties.onSubmitSizeChange}
+      disableMovement={properties.disableMovement}
+      x={properties.x}
+      y={properties.y}
+      posVector={properties.posVector}
+      onSubmitPositionChange={properties.onSubmitPositionChange}
+      onDrag={properties.onDrag}
+    >
+      <div
         id={properties.id}
-        enabled={true}
-        className={properties.className}
-
-        enableResizing={properties.enableResizing}
-        width={properties.width}
-        heigth={properties.heigth}
-        sizeVector={properties.sizeVector}
-        autoBreakOverMultiplePages={properties.autoBreakOverMultiplePages}
-        onResize={properties.onResize}
-        onSubmitSizeChange={properties.onSubmitSizeChange}
-
-        disableMovement={properties.disableMovement}
-        x={properties.x}
-        y={properties.y}
-        posVector={properties.posVector}
-        onSubmitPositionChange={properties.onSubmitPositionChange}
-        onDrag={properties.onDrag}
+        style={{
+          width: "100%",
+          height: "100%",
+          minHeight: "100%",
+          maxHeight: "100%",
+        }}
       >
-        <div id={properties.id} style={{width: "100%", height: "100%", minHeight: "100%", maxHeight: "100%"}}>
-          <div>
-            <Table>
-              <thead>
-                <tr>
-                  {properties.collums.map((value) => {
-                    return(
-                      <ResizableColumnHeader
-                        key={value.accessor}
-                        width={colWidths[value.accessor]}
-                        col={{accessor: value.accessor, label: value.label}}
-                        onResize={handleResize}
-                      />
-                    );
-                  })
-
-                  }
-                </tr>
-              </thead>
-            <tbody>
-            </tbody>
-            </Table>
-          </div>
+        <div>
+          <Table>
+            <thead>
+              <tr>
+                {properties.collums.map((value) => {
+                  return (
+                    <ResizableColumnHeader
+                      key={value.accessor}
+                      width={colWidths[value.accessor]}
+                      col={{ accessor: value.accessor, label: value.label }}
+                      onResize={handleResize}
+                    />
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </Table>
         </div>
-      </BaseMovableBox>
-    );
+      </div>
+    </BaseMovableBox>
+  );
 }
